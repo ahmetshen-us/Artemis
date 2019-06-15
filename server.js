7d54baff06b99ae26cce1ad2c807000da62460b4
Ahmet Sen <ahmetshen.us@gmail.com>
2019-06-15 18:40:22 -0400

    get call to server

--- server.js
+++ server.js
@@ -3,7 +3,14 @@ const graphqlHTTP = require('express-graphql');
 const { schema } = require('./schema');
 
 
+  
 const app = express();
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 
 app.get('/', (req, res) => {
     res.send('GraphQL is amazing!');
