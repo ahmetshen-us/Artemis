d52146e3e28e7a1082d092b6c7acae5422a9b71f
Alexander Umale <alexander.umale@gmail.com>
2019-06-15 21:43:42 -0400

    deployable

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const { schema } = require('./schema');
   
 const app = express();
 
+app.use(express.static('public'));
+
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
